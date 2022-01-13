90671c2b0ae109bb79cd4cc7ed104c8c84938439
Adam Hsu <projectnba77@gmail.com>
2022-01-13 13:49:04 +0800

    feat: 組件創立

---
+++ src/component/Hello/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+import './index.css'
+
+export default class Hello extends Component {
+    render () {
+        return (
+            <div>
+                <h2 className="hello">Hello First Build !!</h2>
+            </div>
+        )
+    }
+}
---
+++ src/component/Welcome/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+import './index.css'
+export default class Welcome extends Component {
+    render () {
+        return (
+            <div>
+                <h3 className="title">建立組件成功</h3>
+            </div>
+        )
+    }
+}
