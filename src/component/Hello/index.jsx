ed4a77a2fb4a413923753da5c92485aa76a623ec
Adam Hsu <projectnba77@gmail.com>
2022-01-13 14:38:16 +0800

    feat: CSS模組化

--- src/component/Hello/index.jsx
+++ src/component/Hello/index.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from "react";
 
-import './index.css'
+import HelloCSS from './index.module.css'
 
 export default class Hello extends Component {
     render () {
         return (
             <div>
-                <h2 className="hello">Hello First Build !!</h2>
+                <h2 className={HelloCSS.hello}>Hello First Build !!</h2>
             </div>
         )
     }
